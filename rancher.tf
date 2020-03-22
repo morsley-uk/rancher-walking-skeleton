545a5c5fc921bf0315e277091f9c8a62fe9ca0ab
John Morsley <johnmorsley@gmail.com>
2020-03-22 18:58:25 +0000

    First attempt at an EKS cluster.

---
+++ rancher.tf
@@ -0,0 +1,59 @@
+﻿#                _           _         _____                  _               
+#       /\      | |         (_)       |  __ \                | |              
+#      /  \   __| |_ __ ___  _ _ __   | |__) |__ _ _ __   ___| |__   ___ _ __ 
+#     / /\ \ / _` | '_ ` _ \| | '_ \  |  _  // _` | '_ \ / __| '_ \ / _ \ '__|
+#    / ____ \ (_| | | | | | | | | | | | | \ \ (_| | | | | (__| | | |  __/ |   
+#   /_/    \_\__,_|_| |_| |_|_|_| |_| |_|  \_\__,_|_| |_|\___|_| |_|\___|_|   
+#                                                                            
+
+# Get 'admin' token from S3
+
+# https://www.terraform.io/docs/providers/aws/r/s3_bucket.html
+
+data "aws_s3_bucket_object" "admin-token" {
+
+  bucket = var.bucket_name
+  key = "/${var.name}/admin_token.txt"
+
+}
+
+# https://www.terraform.io/docs/providers/rancher2/index.html
+
+provider "rancher2" {
+
+  #version = ""
+  
+  alias = "admin"
+
+  api_url   = "https://rancher.morsley.io"
+  token_key = data.aws_s3_bucket_object.admin-token
+
+}
+
+# https://www.terraform.io/docs/providers/rancher2/d/cluster.html
+
+resource "rancher2_cluster" "walking-skeleton" {
+
+  name = var.cluster_name
+  description = "${var.cluster_name} Kubernetes cluster"
+
+  eks_config {
+    access_key                      = var.access_key
+    secret_key                      = var.secret_key
+    #security_groups                 = var.existing_vpc ? [var.security_group_name] : [""]
+    #service_role                    = var.service_role != "" ? var.service_role : aws_iam_role.eks[0].name
+    #subnets                         = var.existing_vpc ? list(var.subnet_id1, var.subnet_id2, var.subnet_id3) : [""]
+    #ami                             = var.ami_id == "" ? data.aws_ami.distro.id : var.ami_id
+    #associate_worker_node_public_ip = var.associate_worker_node_public_ip
+    #instance_type                   = var.instance_type
+    kubernetes_version              = var.kubernetes_version
+    maximum_nodes                   = 1 #var.maximum_nodes
+    minimum_nodes                   = 1 #var.minimum_nodes
+    #node_volume_size                = var.disk_size
+    region                          = var.region
+    #session_token                   = var.session_token
+    #virtual_network                 = var.vpc_id
+  }  
+  
+}
+
---
+++ variables.tf
@@ -0,0 +1,34 @@
+﻿#   __      __        _       _     _           
+#   \ \    / /       (_)     | |   | |          
+#    \ \  / /_ _ _ __ _  __ _| |__ | | ___  ___ 
+#     \ \/ / _` | '__| |/ _` | '_ \| |/ _ \/ __|
+#      \  / (_| | |  | | (_| | |_) | |  __/\__ \
+#       \/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/
+#                                             
+
+variable "region" {}
+
+variable "name" {}
+
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "bucket_name" {}
+
+#variable "instance_type" {
+#  default = "t2.2xlarge" # 8 vCPUs, 32 GB RAM
+#}
+
+#variable "cluster_size" {
+#  default = 1
+#}
+
+variable "cluster_name" {
+  default = "walking-skeleton"
+}
+
+variable "kubernetes_version" {
+  default = "1.17.0" 
+}
+
+#variable "domain_name" {}
